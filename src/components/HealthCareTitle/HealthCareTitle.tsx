9970bc60004a9f1eb8741e44cff5ac8dea554094
saiani1 <saiani@naver.com>
2022-05-27 13:50:29 +0900

    feat: complete HealthCareTitle

---
+++ src/components/HealthCareTitle/HealthCareTitle.tsx
@@ -0,0 +1,19 @@
+import styles from './healthCareTitle.module.scss';
+import healthData from '../../data/healthData.json';
+
+const HealthCareTitle = () => {
+  const maxHscore = healthData.wxcResultMap.wMymaxHscore;
+
+  return (
+    <div className={styles.wrap}>
+      <h3 className={styles.title}>맞춤 건강관리</h3>
+      <p className={styles.contents}>
+        오케어와 함께 건강을 관리해보세요.
+        <br />
+        건강점수를 <strong className={styles.bold}>최대 {maxHscore}점</strong>까지 올릴 수 있어요.
+      </p>
+    </div>
+  );
+};
+
+export default HealthCareTitle;
