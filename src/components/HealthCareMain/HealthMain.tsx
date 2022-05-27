c7b8317cae179e472d8ee5e6327b7ecc63001e14
AnJuHwan <wnghks0531@gmail.com>
2022-05-27 16:03:02 +0900

    feat: fie chart

--- src/components/HealthCareMain/HealthMain.tsx
+++ src/components/HealthCareMain/HealthMain.tsx
@@ -1,66 +1,46 @@
 import { VictoryAnimation, VictoryLabel, VictoryPie } from 'victory';
 import styles from './healthMain.module.scss';
 import healthData from '../../data/healthData.json';
-import { useEffect, useState } from 'react';
 
 const HealthMain = () => {
-  const [dataState, setDataState] = useState({
-    percent: 25, data: getData(0)
-  })
-
-
-  useEffect(()=> {
-    let percent = 25;
-    const setStateInterval = window.setInterval(() => {
-      percent += (Math.random() * 25);
-      percent = (percent > 100) ? 0 : percent;
-      setDataState({
-        percent, data: getData(percent)
-      });
-    }, 2000);
-  })
-
-  getData(percent) {
-    return [{ x: 1, y: percent }, { x: 2, y: 100 - percent }];
-  }
-
+  const percent = Number(healthData.userInfo.healthScore) / 1000;
+  const getData = () => {
+    return [
+      { x: 1, y: 100 - percent * 100 },
+      { x: 2, y: percent },
+    ];
+  };
 
   return (
     <div className={styles.app}>
-        <h2>김헬스님의 건강점수</h2>
-      <svg viewBox='0 0 400 400' width='100%' height='100%'>
+      <h2>김헬스님의 건강점수</h2>
+      <svg viewBox='0 0 400 400' width='360px' height='100%'>
         <VictoryPie
           standalone={false}
-          animate={{ duration: 1000 }}
+          // animate={{ duration: 1000 }}
           width={400}
           height={400}
-          data={healthData.userInfo.healthScore}
+          data={getData()}
           innerRadius={120}
-          cornerRadius={25}
           labels={() => null}
+          endAngle={280}
           style={{
             data: {
               fill: ({ datum }) => {
-                const color = datum.y > 30 ? 'green' : 'red';
-                return datum.x === 1 ? color : 'transparent';
+                const color = 'yellow';
+                return datum.x === 1 ? color : 'rgba(255,255,255,0.3)';
               },
             },
           }}
         />
-        <VictoryAnimation duration={1000} data={healthData.userInfo.healthScore}>
-          {(newProps) => {
-            return (
-              <VictoryLabel
-                textAnchor='middle'
-                verticalAnchor='middle'
-                x={200}
-                y={200}
-                text={`${dataState.percent}%`}
-                style={{ fontSize: 45 }}
-              />
-            );
-          }}
-        </VictoryAnimation>
+        <VictoryLabel
+          textAnchor='middle'
+          verticalAnchor='middle'
+          x={200}
+          y={200}
+          text={healthData.userInfo.healthScore}
+          style={{ fontSize: 45 }}
+        />
       </svg>
       <p>건강검진결과 가져오기 &gt;</p>
     </div>
