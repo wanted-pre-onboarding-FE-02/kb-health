335db82cbf625c07712988cda0b7788db257cffa
hyemin065 <p62517@gmail.com>
2022-05-27 21:01:40 +0900

    fix: 전체적인 css 수정

--- src/pages/HealthPage.tsx
+++ src/pages/HealthPage.tsx
@@ -15,6 +15,9 @@ const { sex, resHeight, age } = heathData.wxcResultMap.paramMap;
 const HealthPage = () => {
   return (
     <div className={styles.app}>
+      <header>
+        <h1>마이헬스</h1>
+      </header>
       <div className={styles.inner}>
         <HealthMain />
         <BasicInfo sex={sex} resHeight={resHeight} age={age} />
