67bfef585b99679f229b86c49eea908b224b4bef
minimin123 <kmmin727@gmail.com>
2022-05-27 20:00:09 +0900

    feat: 이전년도 비교 그래프

--- src/pages/HealthPage.tsx
+++ src/pages/HealthPage.tsx
@@ -1,5 +1,6 @@
 import { Icon1 } from '../assets';
 import HealthCareCard from '../components/HealthCareCard/HealthCareCard';
+import HealthScoreYearOnYear from '../components/HealthScoreYearOnYear/healthScoreYearOnYear';
 import styles from './healthPage.module.scss';
 
 const tipList = [
@@ -7,21 +8,7 @@ const tipList = [
   '전체적인 에너지 섭취는 줄이되 영양적 적절성은 유지하는 개인별 영양평가에 기반을 둔 식이요법과 하루 30분 이상 주 5회 이상 중등도 강도의 유산소 운동을 기본으로 하되 점진적으로 하루 1시간, 고강도 운동을 포함하는 유산소 운동 및 주 2-3회의 근력운동을 포함하는 운동 요법이 병행되어야 합니다. 또한 음주 습관, 야식, 폭식 등 관련된 행동 습관도 파악하여 조절해야 합니다.',
 ];
 const HealthPage = () => {
-  return (
-    <div className={styles.app}>
-      <HealthCareCard
-        tagText={['#현상유지', '#과제충']}
-        cardNum='01'
-        icon={<Icon1 />}
-        title='체질량 지수'
-        desc='체질량 지수는 24.8kg/m&sup2;로 입니다.'
-        bordText='과체중'
-        healthState='정상 : 20 ~ 22kg m&sup2;'
-        tipList={tipList}
-        textColor='color1'
-      />
-    </div>
-  );
+  return <HealthScoreYearOnYear />;
 };
 
 export default HealthPage;
